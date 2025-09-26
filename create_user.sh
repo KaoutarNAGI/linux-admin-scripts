3b588d3905e34ecb61ee283b1534aa48bd6a79fc
KaoutarNAGI <kaoutar1301@gmail.com>
2025-09-26 13:57:11 +0200

    Update create_user.sh

--- create_user.sh
+++ create_user.sh
@@ -1,1 +1,26 @@
 
+Exemple `create_user.sh` :
+
+```bash
+#!/bin/bash
+set -e
+
+if [ "$(id -u)" -ne 0 ]; then echo "Run as root"; exit 1; fi
+
+USERNAME="$1"
+FULLNAME="$2"
+ADD_SUDO="$3"
+
+if [ -z "$USERNAME" ]; then
+  echo "Usage: $0 username \"Full Name\" [--sudo]"
+  exit 1
+fi
+
+adduser --gecos "$FULLNAME" --disabled-password "$USERNAME"
+echo "User $USERNAME created."
+
+if [ "$ADD_SUDO" == "--sudo" ]; then
+  usermod -aG sudo "$USERNAME"
+  echo "$USERNAME added to sudo group."
+fi
+
